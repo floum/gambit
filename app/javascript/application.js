f6446e6289c9a2d012744784fde14fa58870f0d4
Efflam Castel <efflamm.castel@gmail.com>
2024-11-02 20:38:51 +0000

    chessground imports and runs

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "popper"
 import "bootstrap"
+
+import 'chessground';
+import { Chessground } from 'chessground'
+
+let board = document.getElementById('chessboard')
+
+Chessground(board, {})
