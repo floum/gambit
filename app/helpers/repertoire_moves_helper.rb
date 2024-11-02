47957a8fb4d627d18c2b0e8f040438914de4dfd3
Efflam Castel <efflamm.castel@gmail.com>
2024-11-02 21:54:36 +0000

    repertoire move forms

---
+++ app/helpers/repertoire_moves_helper.rb
@@ -0,0 +1,2 @@
+module RepertoireMovesHelper
+end
--- app/models/repertoire_move.rb
+++ app/models/repertoire_move.rb
@@ -1,4 +1,5 @@
 class RepertoireMove < ApplicationRecord
   belongs_to :move
   belongs_to :repertoire_position
+  has_one :repertoire, through: :repertoire_position
 end
