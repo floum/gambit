8d4ba955e493804cb6c6d172f51716ca8d15f4d7
efflam castel <efflam.castel@orange.com>
2025-01-16 15:44:30 +0100

    removing dead code from moves

--- app/models/move.rb
+++ app/models/move.rb
@@ -1,15 +1,2 @@
 class Move < ApplicationRecord
-  def popularity
-    count.to_f / position.count
-  end
-
-  def resulting_position
-    resulting_fen = Stockfish.new.compute_move(fen, uci)
-    Position.find_or_create_by(fen: resulting_fen)
-  end
-
-  def create_result
-    self.result = resulting_position
-    save
-  end
 end
