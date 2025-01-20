a9d3ac4ef8ce3f22d01ee770e85e6d4aa7d56025
efflam castel <efflam.castel@orange.com>
2025-01-20 15:05:35 +0100

    feat(repertoires): destroying with confirmation

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,4 @@ pin "popper", to: 'popper.js', preload: true
 pin "bootstrap", to: 'bootstrap.min.js', preload: true
 pin "chessground" # @9.1.1
 pin "chess.js" # @1.0.0
+pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
