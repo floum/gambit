118958151e6a2b19384eed6c03e98743956e9407
Efflam Castel <efflamm.castel@gmail.com>
2025-01-20 11:32:57 +0000

    adding fen param to edit

--- app/models/move.rb
+++ app/models/move.rb
@@ -1,2 +1,13 @@
 class Move < ApplicationRecord
+  def sheet_notation
+    "#{number}.#{ color == 'b' ? '..' : ''}#{san}"
+  end
+
+  def number
+    before.split(' ').last
+  end
+
+  def color
+    before.split(' ')[1]
+  end
 end
