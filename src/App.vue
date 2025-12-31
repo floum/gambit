601e2ab636a2470f09b5807cdb941565c4d0183f
Efflam Castel <efflamm.castel@gmail.com>
2026-01-01 00:30:55 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import { TheChessboard } from 'vue3-chessboard';
+  import 'vue3-chessboard/style.css';
+</script>
+
+<template>
+  <h1>Gambit</h1>
+  <TheChessboard />
+</template>
+
+<style>
+  body {
+    background-color: black;
+    color: white;
+    text-align: center;
+  }
+</style>
