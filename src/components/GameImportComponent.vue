341b9638d27c7a9e0536f767db2ebde43789f2b6
Efflam Castel <efflamm.castel@gmail.com>
2026-01-04 16:17:12 +0100

    feature: game moves

--- src/components/GameImportComponent.vue
+++ src/components/GameImportComponent.vue
@@ -1,18 +1,24 @@
 <script setup>
 import { ref } from 'vue'
+import { Chess } from 'chess.js'
 let pgn = ref(null)
+let game = new Chess()
 
 const save = async () => {
+    game.loadPgn(pgn.value)
+    let history = game.history({ verbose: true })
     const response = await fetch(
         'http://192.168.1.22:3000/games',
         {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ pgn: pgn.value })
+            body: JSON.stringify({
+                game: { pgn: pgn.value, moves: history }
+            })
         }
     )
     const data = await response.json()
-    
+
 }    
 </script>
 
