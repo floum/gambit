53377bbbcc5770c4f7321d7d778e7c775586e41c
Efflam Castel <efflamm.castel@gmail.com>
2026-01-05 10:36:08 +0100

    UI: study index improvements

--- src/components/StudyIndexComponent.vue
+++ src/components/StudyIndexComponent.vue
@@ -7,12 +7,14 @@ let studies = ref([]);
 onMounted(async () => {
     const response = await fetch('http://192.168.1.22:3000/game_studies')
     studies.value = await response.json()
-    console.log(studies)
 })
 
 const select = (id) => {
-    console.log('Selected: '+ id)
     emit('selected', id)
+}
+
+const gameName = (game) => {
+    return `${game.white} - ${game.white}`
 }
 </script>
 
@@ -21,8 +23,8 @@ const select = (id) => {
     <template v-for="study in studies">
         <tr>
         <td>{{ study.id }}</td>
-        <td>{{ study.game_id }}</td>
-        <td><button @click="select(study.id)">Select</button></td>
+        <td>{{ gameName(study.game) }}</td>
+        <td><button class="btn-gambit" @click="select(study.id)">Select</button></td>
         </tr>
     </template>
     </table>
