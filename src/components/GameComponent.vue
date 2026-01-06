24d39dbd4367abed5d263a39e67c8690869d0226
Efflam Castel <efflamm.castel@gmail.com>
2026-01-06 14:51:34 +0100

    UI: Game history improvements & fixes

--- src/components/GameComponent.vue
+++ src/components/GameComponent.vue
@@ -1,58 +1,39 @@
 <script setup>
-import { ref, onMounted, watch, computed } from 'vue'
+import { ref, computed } from 'vue'
 import { TheChessboard } from 'vue3-chessboard';
-import { Chess } from 'chess.js'
 import { fetchGame } from '../assets/api.js'
 
 const props = defineProps(['id'])
 
 let boardAPI;
-let gameLoaded = ref(false)
-let gameData = ref({})
-
-const pgn = ref('')
-const game = new Chess()
-let index = 0
-
-const loadGame = async() => {
-    gameData = await fetchGame(props.id)
-    game.loadPgn(gameData.pgn)
-    gameLoaded.value = true
-}
+let game = ref({})
 
 const gameName = computed(() => {
-    return `${gameData.white} - ${gameData.black} ${gameData.result}`
-})
-
-onMounted(loadGame)
-
-watch(props, async() => {
-    loadGame()
-    index = 0
-    boardAPI.setPosition(game.history({ verbose: true })[index].before)
+    return `${game.value.white} - ${game.value.black} ${game.value.result}`
 })
 
 const next = () => {
-    index++
-    boardAPI.setPosition(game.history({ verbose: true })[index].before)
+    boardAPI.viewNext()
 }
 
 const previous = () => {
-    index--
-    if (index <= 0) {
-        index = 0
-    }
-    boardAPI.setPosition(game.history({ verbose: true })[index].before)
+    boardAPI.viewPrevious()
 }
 
-
+const loadGame = async(api) => {
+    boardAPI = api
+    game.value = await fetchGame(props.id)
+    boardAPI.loadPgn(game.value.pgn)
+    boardAPI.viewHistory()
+    boardAPI.viewStart()
+}
 </script>
 
 <template>
-    <template v-if="gameLoaded">
+    <template v-if="props.id">
         <h2>{{ gameName }}</h2>
-        <TheChessboard @board-created="(api) => (boardAPI = api)" />
-        <button @click="previous">Previous</button>
-        <button @click="next">Next</button>
+        <TheChessboard @board-created="loadGame" />
+        <button class="btn-gambit" @click="previous">< Previous</button>
+        <button class="btn-gambit" @click="next">Next ></button>
     </template>
 </template>
