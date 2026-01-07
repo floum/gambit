6aa322cbf6f5386efb91606826d79a48d337e903
Efflam Castel <efflamm.castel@gmail.com>
2026-01-07 16:39:04 +0100

    UI: consumable study results

--- src/components/StudyResultsComponent.vue
+++ src/components/StudyResultsComponent.vue
@@ -1,15 +1,11 @@
 <script setup>
-import { computed } from 'vue'
-import SuccessComponent from './SuccessComponent.vue'
+import { ref, computed } from 'vue'
+import { TheChessboard } from 'vue3-chessboard'
 
 const props = defineProps(['study'])
-
-console.log(props.study.game_study_moves)
-const matches = computed(() => {
-    return props.study.expected_moves.reduce((count, expected_move, index)=> {
-        return count + (expected_move.san == props.study.game_study_moves[index].san ? 1 : 0)
-    }, 0)
-})
+let boardConfig = {}
+let boardAPI
+let currentMove = ref(null)
 
 const moves = computed(() => {
     return props.study.expected_moves.map((expected_move, index) => {
@@ -20,30 +16,51 @@ const moves = computed(() => {
     })
 })
 
-const success = (move) => {
-    return move.expected.san == move.submitted.san
+const difference = (move) => {
+    return move.submitted.evaluation - move.expected.evaluation
 }
 
+const view = (move) => {
+    currentMove.value = move
+    if (boardAPI) {
+        boardAPI.setPosition(currentMove.value.expected.before)
+    }
+}
+
+const loadBoard = (api) => {
+    boardAPI = api
+    boardAPI.setPosition(currentMove.value.expected.before)
+}
 </script>
 
 <template>
     <div>Results</div>
-    <div>Matches : {{ matches }}</div>
-    <table>
-        <thead>
-            <th>GM Move</th>
-            <th>evaluation</th>
-            <th>Your Move</th>
-            <th>evaluation</th>
-        </thead>
-        <template v-for="move in moves">
-            <tr>
-                <td>{{ move.expected.san }}</td>
-                <td>{{ move.expected.evaluation }}</td>
-                <td>{{ move.submitted.san }}</td>
-                <td>{{ move.submitted.evaluation }}</td>
-                <td><SuccessComponent v-if="success(move)"></SuccessComponent></td>
-            </tr>
-        </template>
-    </table>
+    <div id="grid">
+        <div id="board">
+            <div v-if="currentMove">
+                <TheChessboard @board-created="loadBoard" :board-config="boardConfig" />
+                <div>Played : {{ currentMove.expected.san }}</div>
+                <div>Submitted : {{ currentMove.submitted.san }}</div>
+            </div>
+        </div>
+        <table>
+            <thead>
+                <th>GM Move</th>
+                <th>evaluation</th>
+                <th>Your Move</th>
+                <th>evaluation</th>
+            </thead>
+            <template v-for="move in moves">
+                <tr>
+                    <td>{{ move.expected.san }}</td>
+                    <td>{{ move.expected.evaluation }}</td>
+                    <td>{{ move.submitted.san }}</td>
+                    <td>{{ move.submitted.evaluation }}</td>
+                    <td>{{ difference(move) }}</td>
+                    <td><button @click="view(move)">View</button></td>
+                    <td></td>
+                </tr>
+            </template>
+        </table>
+    </div>
 </template>
