7251f0e328307712f91156ab3d3c7a5884945cb7
Efflam Castel <efflamm.castel@gmail.com>
2026-04-06 03:37:35 +0200

    barely running practice

--- src/components/MoveTrainerComponent.vue
+++ src/components/MoveTrainerComponent.vue
@@ -16,6 +16,9 @@ const handleMove = (move) => {
         expectedMove: expected_move,
         success: expected_move.san == move.san
     })
+}
+
+const next = () => {
     index++
     if (index < props.moves.length) {
         boardAPI.setPosition(props.moves[index].fen)
@@ -27,5 +30,6 @@ const handleMove = (move) => {
 <template>
     <template v-if="index < props.moves.length">
         <TheChessboard @board-created="(api) => (boardAPI = api)" @move="handleMove"></TheChessboard>
+        <button @click="next">Next</button>
     </template>
 </template>
